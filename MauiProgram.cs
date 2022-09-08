b8efd715c8151951972bf9594e4318555955ad8a
Andreas Aronsen Nesheim <andreas.nesheim@capgemini.com>
2022-09-08 22:27:33 +0200

    changes to make project build

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,18 +1,52 @@
-﻿namespace MauiFirebasePush;
+﻿using Microsoft.Maui.LifecycleEvents;
+using Plugin.Firebase.Auth;
+using Plugin.Firebase.Shared;
+#if IOS
+using Plugin.Firebase.iOS;
+#else
+using Plugin.Firebase.Android;
+#endif
+
+namespace MauiFirebasePush;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .RegisterFirebaseServices()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
 
-		return builder.Build();
-	}
+        return builder.Build();
+    }
+
+    private static MauiAppBuilder RegisterFirebaseServices(this MauiAppBuilder builder)
+    {
+        builder.ConfigureLifecycleEvents(events =>
+        {
+#if IOS
+            events.AddiOS(iOS => iOS.FinishedLaunching((app, launchOptions) => {
+                CrossFirebase.Initialize(app, launchOptions, CreateCrossFirebaseSettings());
+                return false;
+            }));
+#else
+            events.AddAndroid(android => android.OnCreate((activity, state) =>
+                CrossFirebase.Initialize(activity, state, CreateCrossFirebaseSettings())));
+#endif
+        });
+
+        builder.Services.AddSingleton(_ => CrossFirebaseAuth.Current);
+        return builder;
+    }
+
+    private static CrossFirebaseSettings CreateCrossFirebaseSettings()
+    {
+        return new CrossFirebaseSettings(isAuthEnabled: true);
+    }
 }
